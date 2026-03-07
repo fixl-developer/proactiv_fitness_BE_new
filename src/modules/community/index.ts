7f14fbd39493663e5e7ba0ae788c769ac7646cf4
fixl-developer <develop@fixlsolutions.com>
2026-03-07 10:31:03 +0530

    chore: isolate phase

--- src/modules/community/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './community.interface';
-export * from './community.service';
-export * from './community.controller';
-export { default as communityRoutes } from './community.routes';
